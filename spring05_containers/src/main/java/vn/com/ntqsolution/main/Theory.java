b0b5f06e7ee66d8601e2c9e6a73d6f76d783defe
truongbb <truongbb96@outlook.com>
2019-12-21 23:15:05 +0700

    20191221_truongbb: commit lesson 5

--- spring05_containers/src/main/java/vn/com/ntqsolution/main/Theory.java
+++ spring05_containers/src/main/java/vn/com/ntqsolution/main/Theory.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import vn.com.ntqsolution.bean.Clazz;
 import vn.com.ntqsolution.bean.Student;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class Theory {
     /**

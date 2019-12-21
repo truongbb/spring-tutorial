f45d96583463800ecf33dfce9dd69bc268161c7e
truongbb <truongbb96@outlook.com>
2019-12-21 20:27:39 +0700

    20191221_truongbb: commit lesson 3

---
+++ spring03_beanPart2/src/main/java/vn/com/ntqsolution/main/DemoParentBean.java
@@ -0,0 +1,31 @@
+package vn.com.ntqsolution.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import vn.com.ntqsolution.bean.Person;
+import vn.com.ntqsolution.bean.Student;
+
+public class DemoParentBean {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Person person = (Person) context.getBean("person");
+        System.out.println(person);
+
+        Person student = (Student) context.getBean("student");
+        System.out.println(student);
+
+        Person student1 = (Student) context.getBean("student1");
+        System.out.println(student1);
+
+        /**
+         * Một ví dụ nhỏ demo cho thuộc tính parent khi config một bean
+         *
+         * Các quan hệ kế thừa của bean cũng được thể hiện rõ rệt khi mà ta cấu hình bean đó, và khi set giá trị cho các thuộc tính của bean
+         * khi xem kết quả có thể thấy được giá trị các thuộc tính chung như phone hay name của student sẽ set đè lên person khi khởi tạo student
+         *
+         */
+    }
+
+}

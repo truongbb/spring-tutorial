107ff72940a245f2773d5fc54fd4d304a992f644
truongbb <truongbb96@outlook.com>
2019-12-22 15:55:36 +0700

    20191222_truongbb: commit lesson 6

---
+++ spring06_autowiring/src/main/java/vn/com/ntqsolution/bean/Student.java
@@ -0,0 +1,19 @@
+package vn.com.ntqsolution.bean;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Student extends Person {
+
+    private Clazz clazz;
+    private float avgMark;
+
+    public Student(Clazz clazz) { // nếu không có constructor này thì bean Clazz sẽ khong được inject theo kiểu constructor
+        this.clazz = clazz;
+    }
+}
